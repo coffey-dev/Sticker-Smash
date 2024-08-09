4d830401bedfed5383a8a93cdf26ff76c8df6aa1
coffey-dev <scoffey@abc.gob.ar>
2024-08-09 13:13:37 -0300

    Agrego grabar a version web

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { useState, useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
+
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -57,6 +59,7 @@ export default function App() {
 
 
   const onSaveImageAsync = async () => {
+    if (Platform.OS !== 'web') {
     try {
       const localUri = await captureRef(imageRef, {
         height: 440,
@@ -70,7 +73,24 @@ export default function App() {
     } catch (e) {
       console.log(e);
     }
-  };
+  }
+  else {
+    try {
+      const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+        quality: 0.95,
+        width: 320,
+        height: 440,
+      });
+
+      let link = document.createElement('a');
+      link.download = 'sticker-smash.jpeg';
+      link.href = dataUrl;
+      link.click();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+};
 
   return (
     <GestureHandlerRootView style={styles.container}>
